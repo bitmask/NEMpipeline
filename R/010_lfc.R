3bec319ffd4a424b5de2f235ad3586bd528a5a86
Helen Cook <helen.cook@gmail.com>
2019-05-03 12:36:58 +0100

    test

--- R/010_lfc.R
+++ R/010_lfc.R
@@ -5,35 +5,36 @@
 
 source("config.R")
 
-#' Compute lfc from bam files
+#' Compute log fold change from bam files
 #'
 #' @param project
 #' @param aligners
 #' @param sampleTable
 #' @param base_input_dir
 #' @param lfc_dir
-#' @return Number representing the distance between x and y
+#' @return nothing. writes output to file
 #' @export
 
 step_010_lfc <- function(project, aligners, sampleTable, base_input_dir, lfc_dir) {
     for (aligner in aligners) {
+        print(aligner)
         # set up the input/output locations
-        align_dir <- file.path(base_input_dir, project, "aligned", aligner)
-
-        # loading bam.files
-        filenames <- as.character(sampleTable$Bam.File)
-        filenames <- paste(align_dir, filenames, sep="/")
-        print(filenames)
-        file.exists(filenames)
-        bamfiles <- Rsamtools::BamFileList(filenames)
-        seqinfo(bamfiles[1])
-              
-        # set to single core
-        #register(SerialParam())
-              
-        # counting reads
-        fc <- Rsubread::featureCounts(filenames,annot.inbuilt="hg38",isPairedEnd=TRUE)
-
-        saveRDS(fc, file.path(lfc_dir, paste(aligner, project, "Rds", sep=".")))
+#        align_dir <- file.path(base_input_dir, project, "aligned", aligner)
+#
+#        # loading bam.files
+#        filenames <- as.character(sampleTable$Bam.File)
+#        filenames <- paste(align_dir, filenames, sep="/")
+#        print(filenames)
+#        file.exists(filenames)
+#        bamfiles <- Rsamtools::BamFileList(filenames)
+#        seqinfo(bamfiles[1])
+#              
+#        # set to single core
+#        #register(SerialParam())
+#              
+#        # counting reads
+#        fc <- Rsubread::featureCounts(filenames,annot.inbuilt="hg38",isPairedEnd=TRUE)
+#
+#        saveRDS(fc, file.path(lfc_dir, paste(aligner, project, "Rds", sep=".")))
     }
 }
