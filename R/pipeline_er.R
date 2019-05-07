693c4eea78a2bdb24b4f31a5e4ebce764849284f
Helen Cook <helen.cook@gmail.com>
2019-05-07 17:33:29 +0100

    add arg

--- R/pipeline_er.R
+++ R/pipeline_er.R
@@ -66,11 +66,10 @@ expr.cutoff <- 0.5
 #
 ################################################################################
 
-print("definitions")
-print(paste("project ", project))
-print(paste("aligner ", aligner))
-print(paste("exp_def", head(ER_experiment_definitions)))
+print("Running ER pipeline")
+print(paste("project: ", project))
+print(paste("aligner: ", aligner))
     step_010_lfc(project, aligner, ER_experiment_definitions, base_input_dir, lfc_dir)
-    step_030_diffexp(project, aligner, diffexp_method, lfc_dir, diffexp_dir, ER_experiment_definitions)
+    step_030_diffexp(project, aligner, diffexp_method, lfc_dir, diffexp_dir, ER_experiment_definitions, expr.cutoff)
 }
 
