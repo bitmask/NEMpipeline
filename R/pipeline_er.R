cec9e2af41d126a27c2faae6c2f3c23f82f2757c
Helen Cook <helen.cook@gmail.com>
2019-05-09 17:45:48 +0100

    prep_method

--- R/pipeline_er.R
+++ R/pipeline_er.R
@@ -56,6 +56,20 @@ run_ER_pipeline <- function() {
     # diffexp_methods: DESeq, edgeR
     diffexp_method <- "DESeq" 
 
+    # prep_methods: binary, lfc, pvalue, boolean, fgnem, decompose, progressive, bootstrap, random, geneset, cluster_bin
+    # binary - discretize expression data to 0 or 1 (there is an effect or not)
+    # lfc - use log fold change of differential expression
+    # pvalue - use pvalue of differential expression
+    # boolean - model inhibition and activation with bnem
+    # fgnem - use factor graph nem
+    # decompose - subset sgenes into a set of smaller nems, then evaluate with search to see if the models are compositional
+    # progressive - take N most significant egenes, then 2N most significant, then 3N etc
+    # bootstrap - randomly sample from the egenes
+    # random - randomly generate data, to compare the bootstraps against
+    # geneset - use gsea to group the egenes into genesets to run the nems on
+    # cluster_bin - cluster binary expression data to generate ensemble egenes to run nems on
+    prep_method <- "binary"
+
     # successful screens
     samples <- c("EP300 12BR3 A", "EP300 12BR3 B", "EP300 12BR3 C", "ESRRA 2C A", "ESRRA 2C B", "ESRRA 2C C", "NCOA3 1DR3 A", "NCOA3 1DR3 B", "NCOA3 1DR3 C", "NCOA3 2BR A", "NCOA3 2BR B", "NCOA3 2BR C", "NR2F2 17AR3 A", "NR2F2 17AR3 B", "NR2F2 17AR3 C", "NRIP1 5C A", "NRIP1 5C B", "NRIP1 6A C", "RARA 7B A", "RARA 8C A", "RARA 8C C", "SUMO1 M1 A", "SUMO1 M1 B",     "SUMO1 M1 C",    "SUMO1 M35 A",    "SUMO1 M35 B",    "SUMO1 M35 C", "SUMO3 24AR3 A", "SUMO3 24AR3 B", "SUMO3 24AR3 C", "SUMO3 M16 A", "SUMO3 M16 C", "TRIM33 13CR3 A", "TRIM33 13CR3 B", "TRIM33 13CR3 C",     "TRIM33 M2 B", "TRIM33 M2 C", "ZMIZ1 19AR3 A", "ZMIZ1 19AR3 B", "ZMIZ1 19AR3 C", "ZMIZ1 22m35b A", "ZMIZ1 22m35b B", "ZMIZ1 22m35b C")
 
